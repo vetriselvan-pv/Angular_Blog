11420d35d09ea0702381fa102a36fe5b462e6527
Vetriselvan Panneerselvam <85449760+vetriselvan-pv@users.noreply.github.com>
2026-03-10 12:26:41 +0530

    ng-diagram init

--- projects/remoteApp/src/app/app.config.ts
+++ projects/remoteApp/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideNgDiagram } from 'ng-diagram';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- projects/remoteApp/src/app/app.ts
+++ projects/remoteApp/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initializeModel, ModelAdapter, NgDiagramComponent, provideNgDiagram } from 'ng-diagram';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgDiagramComponent],
+  providers : [ provideNgDiagram()],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('remoteApp');
+
+  protected readonly model = initializeModel({
+    nodes: [
+      { id: '1', position: { x: 100, y: 150 }, data: { label: 'Node 1' } },
+      { id: '2', position: { x: 400, y: 150 }, data: { label: 'Node 2' } },
+    ],
+    edges: [
+      {
+        id: '1',
+        source: '1',
+        sourcePort: 'port-right',
+        targetPort: 'port-left',
+        target: '2',
+        data: {},
+      },
+    ],
+  });
 }
